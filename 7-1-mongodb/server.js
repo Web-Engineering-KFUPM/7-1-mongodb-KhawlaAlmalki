426bf22ef4be6c1319dc5619481b0a4884cf1e35
KhawlaAlmalki <s202247320@kfupm.edu.sa>
2025-11-17 11:42:04 +0300

    TODO 1,2,3

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -193,6 +193,16 @@ const Student = mongoose.model("Student", studentSchema);
 
 
 // create document
+async function createStudents() {
+    await Student.insertMany([
+        { name: "Ali", age: 21, major: "CS" },
+        { name: "Sara", age: 23, major: "SE" }
+    ]);
+
+    console.log("✅ Inserted");
+}
+
+createStudents();
 
 
 // read document
